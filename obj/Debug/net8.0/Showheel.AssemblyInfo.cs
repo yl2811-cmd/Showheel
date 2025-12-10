acf4a85c15c0b63b1e2ed81b6972b1143cacf1e0
yl2811-cmd <yl2811@g.rit.edu>
2025-12-09 16:46:39 -0800

    Remove local render assets and use external images

--- obj/Debug/net8.0/Showheel.AssemblyInfo.cs
+++ obj/Debug/net8.0/Showheel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba64b73852f37b3d61466bacf5f15479b808151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe2df1d01f98f3d8a553209ecd8a49c3db704f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

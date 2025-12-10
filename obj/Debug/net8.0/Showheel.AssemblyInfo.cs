d8780083cc67f1da8f8210ffea0f65f09efbfdca
yl2811-cmd <yl2811@g.rit.edu>
2025-12-09 20:20:48 -0800

    Tighten hero layout spacing

--- obj/Debug/net8.0/Showheel.AssemblyInfo.cs
+++ obj/Debug/net8.0/Showheel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73bf4d2de717d788ec9bf98ca94b8e61395ad6df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0865909a844be65d6bda408638b11001eee169f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

fd5c9e4a622ce31a969576d2e2381e248ddf95ea
yl2811-cmd <yl2811@g.rit.edu>
2026-07-02 05:18:03 +0800

    34

--- obj/Debug/net8.0/Showheel.AssemblyInfo.cs
+++ obj/Debug/net8.0/Showheel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a25aa2f5de3e12f1f1cc42f1bfb77a57b76612")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14aa873947e16a8d498ad70f8f2c8ed50d96a12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

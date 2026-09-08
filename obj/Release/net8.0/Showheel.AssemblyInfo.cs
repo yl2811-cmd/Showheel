fec73b2e266fe03a916f8e573fcb83b2ffbb2dfc
yl2811-cmd <yl2811@g.rit.edu>
2026-09-08 03:49:18 -0400

    Update story.md

--- obj/Release/net8.0/Showheel.AssemblyInfo.cs
+++ obj/Release/net8.0/Showheel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c131c2e26f6291541dde019a22ea101c176972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea891b327714b0f1a4a2f5084a0175df564ebede")]
 [assembly: System.Reflection.AssemblyProductAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

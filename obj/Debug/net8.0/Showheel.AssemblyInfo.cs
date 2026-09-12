0b38a457597321f6b040c5b450fe0f61ab214faa
yl281 <yl281@ANNAFREEDMAN>
2026-09-11 20:19:38 -0400

    Vary Atheria houses with disjoint enlargement and compound additions

--- obj/Debug/net8.0/Showheel.AssemblyInfo.cs
+++ obj/Debug/net8.0/Showheel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14dd5775127cb2c38d2d87d01914d4fc8c49b3f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be40ff5b51aeb9316b190cb2fde3986da271dbbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Showheel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
